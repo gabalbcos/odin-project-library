6e1bc5d86951290a99eafe6388e533a17cb69966
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-04-16 12:03:40 -0300

    Add initial form design

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-let myLibrary = [];
+let myLibrary = [
+  {
+      "title": "asdasd",
+      "author": "asdas",
+      "pages": "123"
+  },
+  {
+      "title": "lkjh",
+      "author": "kjh",
+      "pages": "765"
+  }
+];
 
-
+// select the form to get the values
 const form = document.getElementById('contact-form');
+
+// dictate what happens after the submit button is pushed
+// tells where the info will go
 
 form.addEventListener('submit', addBookToLibrary);
 
 function addBookToLibrary(event) {
-  // do stuff here
+  // cancel auto refresh
   event.preventDefault();
+
+  // new obj using constructor
   const myFormData = new FormData(event.target);
   
   const formDataObj = {};
   myFormData.forEach((value, key) => (formDataObj[key] = value));
-  console.log(formDataObj);
+  myLibrary.push(formDataObj);
 }
+
+// button to open form
+const FORM = document.querySelector('.new_book_form');
+const BTN_OPEN_FORM = document.querySelector('.add_book');
+
+function toggleFormVisibility() {
+  FORM.classList.toggle('hidden');
+}
+
+BTN_OPEN_FORM.addEventListener('click', toggleFormVisibility)
+
