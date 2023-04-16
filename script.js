fc98bb91620b2274f21d2f8ad98de4bc4bbaf226
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-04-16 17:30:54 -0300

    add function to create book card

--- script.js
+++ script.js
@@ -13,6 +13,39 @@ let myLibrary = [
 
 // select the form to get the values
 const form = document.getElementById('contact-form');
+const library = document.querySelector('.library');
+
+// function to create book card
+function createBook() {
+  const div_card = document.createElement('div');
+  div_card.classList.add('book');
+
+  const btn_remove = document.createElement('button');
+  btn_remove.classList.add('btn_remove');
+  btn_remove.textContent = 'x';
+  div_card.appendChild(btn_remove);
+
+
+  const title = document.createElement('h2');
+  title.classList.add('book_title');
+  title.textContent = 'Hello World!'  
+  div_card.appendChild(title);
+
+  const author = document.createElement('p');
+  author.textContent = 'Hello World2!';
+  div_card.appendChild(author);
+
+  const pages = document.createElement('p');
+  pages.textContent = 'Hello World!3';
+  div_card.appendChild(pages);
+
+  const btn_read = document.createElement('button');
+  btn_read.classList.add('btn_read');
+  btn_read.textContent = 'Hello World!4';
+  div_card.appendChild(btn_read);
+
+  library.appendChild(div_card);
+}
 
 // dictate what happens after the submit button is pushed
 // tells where the info will go
@@ -40,4 +73,6 @@ function toggleFormVisibility() {
 }
 
 BTN_OPEN_FORM.addEventListener('click', toggleFormVisibility)
+
+// test
 
