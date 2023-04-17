2fb213ce4446dd1ddc6cfa19e9dcdd7319df8606
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-04-17 09:04:57 -0300

    organize code style

--- script.js
+++ script.js
@@ -1,105 +1,98 @@
 let myLibrary = [
-  {
-      "title": "asdasd",
-      "author": "asdas",
-      "pages": "123"
-  },
-  {
-      "title": "lkjh",
-      "author": "kjh",
-      "pages": "765"
-  }
 ];
 
 // select the form to get the values
 const form = document.getElementById('book-form');
-const form_card = document.querySelector('.form_card');
+const formCard = document.querySelector('.form_card');
 const library = document.querySelector('.library');
 
 // function to create book card
 function createBook(array, i) {
-  const div_card = document.createElement('div');
-  div_card.classList.add('book');
+  const divCard = document.createElement('div');
+  divCard.classList.add('book');
 
-  const btn_remove = document.createElement('button');
-  btn_remove.classList.add('btn_remove');
-  btn_remove.innerHTML = '<i class="fa-solid fa-circle-xmark"></i>';
-  btn_remove.setAttribute('onclick', 'deleteBook(this)');
-  div_card.appendChild(btn_remove);
+  const btnRemove = document.createElement('button');
+  btnRemove.classList.add('btn_remove');
+  btnRemove.innerHTML = '<i class="fa-solid fa-circle-xmark"></i>';
+  btnRemove.setAttribute('onclick', 'deleteBook(this)');
+  divCard.appendChild(btnRemove);
 
   const title = document.createElement('h2');
   title.classList.add('book_title');
-  title.textContent = array[i].title;  
-  div_card.appendChild(title);
+  title.textContent = array[i].title;
+  divCard.appendChild(title);
 
   const author = document.createElement('p');
   author.textContent = `Author: ${array[i].author}`;
-  div_card.appendChild(author);
+  divCard.appendChild(author);
 
   const pages = document.createElement('p');
   pages.textContent = `Pages: ${array[i].pages}`;
-  div_card.appendChild(pages);
+  divCard.appendChild(pages);
 
-  const btn_read = document.createElement('button');
-  btn_read.classList.add('btn_read', 'red');
-  btn_read.textContent = 'Not Read';
-  btn_read.setAttribute('onclick', 'toggleRead(this)');
-  div_card.appendChild(btn_read);
+  const btnRead = document.createElement('button');
+  btnRead.classList.add('btn_read', 'red');
+  btnRead.textContent = 'Not Read';
+  btnRead.setAttribute('onclick', 'toggleRead(this)');
+  divCard.appendChild(btnRead);
 
-  library.appendChild(div_card);
+  library.appendChild(divCard);
 }
 
-// dictate what happens after the submit button is pushed
-// tells where the info will go
-
-form.addEventListener('submit', addBookToLibrary);
+// function to add book to library array
 
 function addBookToLibrary(event) {
-  // cancel auto refresh
+// cancel auto refresh
   event.preventDefault();
 
   // new obj using constructor
   const myFormData = new FormData(event.target);
-  
+
   const formDataObj = {};
-  myFormData.forEach((value, key) => (formDataObj[key] = value));
+  myFormData.forEach((value, key) => { (formDataObj[key] = value); });
   myLibrary.push(formDataObj);
 
   createBook(myLibrary, myLibrary.length - 1);
   form.reset();
 }
 
+// add functionality to the add book button
 
+form.addEventListener('submit', addBookToLibrary);
 
-// button to open form
-const FORM = document.querySelector('.new_book_form');
+// select button to open and close form
+
 const BTN_OPEN_FORM = document.querySelector('.add_book');
 const BTN_CLOSE = document.querySelector('.btn_close');
 
+// function to open or close form window
+
 function toggleFormVisibility() {
-  form_card.classList.toggle('hidden');
+  formCard.classList.toggle('hidden');
 }
 
-BTN_OPEN_FORM.addEventListener('click', toggleFormVisibility)
-BTN_CLOSE.addEventListener('click', toggleFormVisibility)
-// test
+// add function to the button
 
+BTN_OPEN_FORM.addEventListener('click', toggleFormVisibility);
+BTN_CLOSE.addEventListener('click', toggleFormVisibility);
+
+// function to delete a book - onclick
 
 function deleteBook(el) {
   const nextP = el.nextSibling;
-  myLibrary = myLibrary.filter(item => item.title !== nextP.innerText);
+  myLibrary = myLibrary.filter((item) => item.title !== nextP.innerText);
   nextP.parentElement.remove();
 }
 
 function toggleRead(el) {
   const buttonRead = el;
-  if (buttonRead.classList.contains("red")){
-  buttonRead.innerText = 'Read';
-  buttonRead.classList.toggle('red');
-  buttonRead.classList.toggle('green');
+  if (buttonRead.classList.contains('red')) {
+    buttonRead.innerText = 'Read';
+    buttonRead.classList.toggle('red');
+    buttonRead.classList.toggle('green');
   } else {
-  buttonRead.innerText = 'Not Read';
-  buttonRead.classList.toggle('red');
-  buttonRead.classList.toggle('green');
+    buttonRead.innerText = 'Not Read';
+    buttonRead.classList.toggle('red');
+    buttonRead.classList.toggle('green');
   }
 }
