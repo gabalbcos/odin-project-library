e795d1fc9a3c127d2b5eb73baa5186fe9cdb2239
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-04-24 17:34:06 -0300

    change the constructor in the js to a class

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let myLibrary = [
-];
+class Book {
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = false; // You can add additional properties to the Book class as needed
+  }
+}
+
+const myLibrary = [];
 
 // select the form to get the values
 const form = document.getElementById('book-form');
@@ -7,92 +15,80 @@ const formCard = document.querySelector('.form_card');
 const library = document.querySelector('.library');
 
 // function to create book card
-function createBook(array, i) {
+function createBook(book) {
   const divCard = document.createElement('div');
   divCard.classList.add('book');
 
   const btnRemove = document.createElement('button');
   btnRemove.classList.add('btn_remove');
   btnRemove.innerHTML = '<i class="fa-solid fa-circle-xmark"></i>';
-  btnRemove.setAttribute('onclick', 'deleteBook(this)');
+  btnRemove.addEventListener('click', () => deleteBook(book));
   divCard.appendChild(btnRemove);
 
   const title = document.createElement('h2');
   title.classList.add('book_title');
-  title.textContent = array[i].title;
+  title.textContent = book.title;
   divCard.appendChild(title);
 
   const author = document.createElement('p');
-  author.textContent = `Author: ${array[i].author}`;
+  author.textContent = `Author: ${book.author}`;
   divCard.appendChild(author);
 
   const pages = document.createElement('p');
-  pages.textContent = `Pages: ${array[i].pages}`;
+  pages.textContent = `Pages: ${book.pages}`;
   divCard.appendChild(pages);
 
   const btnRead = document.createElement('button');
   btnRead.classList.add('btn_read', 'red');
-  btnRead.textContent = 'Not Read';
-  btnRead.setAttribute('onclick', 'toggleRead(this)');
+  btnRead.textContent = book.read ? 'Read' : 'Not Read';
+  btnRead.addEventListener('click', () => toggleRead(book));
   divCard.appendChild(btnRead);
 
   library.appendChild(divCard);
 }
 
 // function to add book to library array
-
 function addBookToLibrary(event) {
-// cancel auto refresh
   event.preventDefault();
-
-  // new obj using constructor
   const myFormData = new FormData(event.target);
 
   const formDataObj = {};
-  myFormData.forEach((value, key) => { (formDataObj[key] = value); });
-  myLibrary.push(formDataObj);
+  myFormData.forEach((value, key) => {
+    formDataObj[key] = value;
+  });
 
-  createBook(myLibrary, myLibrary.length - 1);
+  const newBook = new Book(formDataObj.title, formDataObj.author, formDataObj.pages);
+  myLibrary.push(newBook);
+
+  createBook(newBook);
   form.reset();
 }
 
 // add functionality to the add book button
-
 form.addEventListener('submit', addBookToLibrary);
 
 // select button to open and close form
-
 const BTN_OPEN_FORM = document.querySelector('.add_book');
 const BTN_CLOSE = document.querySelector('.btn_close');
 
 // function to open or close form window
-
 function toggleFormVisibility() {
   formCard.classList.toggle('hidden');
 }
 
 // add function to the button
-
 BTN_OPEN_FORM.addEventListener('click', toggleFormVisibility);
 BTN_CLOSE.addEventListener('click', toggleFormVisibility);
 
 // function to delete a book - onclick
-
-function deleteBook(el) {
-  const nextP = el.nextSibling;
-  myLibrary = myLibrary.filter((item) => item.title !== nextP.innerText);
-  nextP.parentElement.remove();
+function deleteBook(book) {
+  myLibrary.splice(myLibrary.indexOf(book), 1);
+  library.innerHTML = '';
+  myLibrary.forEach(book => createBook(book));
 }
 
-function toggleRead(el) {
-  const buttonRead = el;
-  if (buttonRead.classList.contains('red')) {
-    buttonRead.innerText = 'Read';
-    buttonRead.classList.toggle('red');
-    buttonRead.classList.toggle('green');
-  } else {
-    buttonRead.innerText = 'Not Read';
-    buttonRead.classList.toggle('red');
-    buttonRead.classList.toggle('green');
-  }
+function toggleRead(book) {
+  book.read = !book.read;
+  library.innerHTML = '';
+  myLibrary.forEach(book => createBook(book));
 }
---
+++ script2.js
@@ -0,0 +1,23 @@
+class Book {
+  book(title, author, pages){
+      this.title,
+      this.author,
+      this.pages
+  }
+}
+
+
+// function to add book to library array
+
+function addBookToLibrary(event) {
+    // cancel auto refresh
+      event.preventDefault();
+    
+      // new obj using constructor
+ww
+      myLibrary.push(formDataObj);
+    
+      createBook(myLibrary, myLibrary.length - 1);
+      form.reset();
+    }
+    
